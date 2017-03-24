8df134b7313698624eb355835fa67460e692463b
Waldemar Neto <waldemarnt@gmail.com>
2017-03-24 14:16:40 -0300

    Adds test to check custom wait parameter

--- test/checkpoint-writer.spec.js
+++ test/checkpoint-writer.spec.js
@@ -8,50 +8,93 @@ const Promise = require('bluebird');
 describe('checkpoint writer', function () {
 
   describe('timeout', function () {
-    const harvestApp = {
-      adapter: {
-        update: () => {}
-      }
-    };
-    const fakeDoc = {ts: 1};
-    const checkpointEvent = checkpointWriter.checkpointEvent;
+    context('when using the default config options', () => {
+      const harvestApp = {
+        adapter: {
+          update: () => {}
+        }
+      };
+      const fakeDoc = {ts: 1};
+      const checkpointEvent = checkpointWriter.checkpointEvent;
 
-    let clock;
+      let clock;
 
-    beforeEach(() => {
-      clock = sinon.useFakeTimers();
-      sinon.stub(harvestApp.adapter, 'update');
-      harvestApp.adapter.update.returns(new Promise.resolve(fakeDoc));
-      checkpointWriter.startWriterLoop(harvestApp);
-      checkpointWriter.setWriterLoopStopped(true);
-      checkpointEvent.emit('newCheckpoint', 1, fakeDoc);
+      beforeEach(() => {
+        clock = sinon.useFakeTimers();
+        sinon.stub(harvestApp.adapter, 'update');
+        harvestApp.adapter.update.returns(new Promise.resolve(fakeDoc));
+        checkpointWriter.startWriterLoop(harvestApp);
+        checkpointWriter.setWriterLoopStopped(true);
+        checkpointEvent.emit('newCheckpoint', 1, fakeDoc);
+      });
+
+      afterEach(() => {
+        harvestApp.adapter.update.restore();
+        clock.restore();
+      });
+
+      it('should clean last doc and checkpoint after handled', done => {
+        clock.tick(1);
+        expect(harvestApp.adapter.update.callCount).to.be.eql(1);
+        clock.tick(1);
+        expect(checkpointWriter.getLastDoc()).to.be.undefined;
+        expect(checkpointWriter.getLastCheckpointId()).to.be.undefined;
+        expect(harvestApp.adapter.update.calledOnce).to.be.true;
+
+        done();
+      });
+
+      it('should write a checkpoint in a given interval', done => {
+        clock.tick(1);
+        expect(harvestApp.adapter.update.callCount).to.be.eql(1);
+
+        checkpointEvent.emit('newCheckpoint', 1, fakeDoc);
+        clock.tick(1);
+        expect(harvestApp.adapter.update.callCount).to.be.eql(2);
+
+        done();
+      });
     });
 
-    afterEach(() => {
-      harvestApp.adapter.update.restore();
-      clock.restore();
-    });
+    context('when passing the option eventsReaderDebounceWait', () => {
+      const eventsReaderDebounceDelay = 1000;
+      const harvestApp = {
+        adapter: {
+          update: () => {}
+        },
+        options: {
+          eventsReaderDebounceWait: eventsReaderDebounceDelay
+        }
+      };
+      const fakeDoc = {ts: 1};
+      const checkpointEvent = checkpointWriter.checkpointEvent;
 
-    it('should clean last doc and checkpoint after handled', done => {
-      clock.tick(1);
-      expect(harvestApp.adapter.update.callCount).to.be.eql(1);
-      clock.tick(1);
-      expect(checkpointWriter.getLastDoc()).to.be.undefined;
-      expect(checkpointWriter.getLastCheckpointId()).to.be.undefined;
-      expect(harvestApp.adapter.update.calledOnce).to.be.true;
+      let clock;
 
-      done();
-    });
+      beforeEach(() => {
+        clock = sinon.useFakeTimers();
+        sinon.stub(harvestApp.adapter, 'update');
+        harvestApp.adapter.update.returns(new Promise.resolve(fakeDoc));
+        checkpointWriter.startWriterLoop(harvestApp);
+        checkpointWriter.setWriterLoopStopped(true);
+        checkpointEvent.emit('newCheckpoint', 1, fakeDoc);
+      });
 
-    it('should write a checkpoint in a given interval', done => {
-      clock.tick(1);
-      expect(harvestApp.adapter.update.callCount).to.be.eql(1);
+      afterEach(() => {
+        harvestApp.adapter.update.restore();
+        clock.restore();
+      });
 
-      checkpointEvent.emit('newCheckpoint', 1, fakeDoc);
-      clock.tick(1);
-      expect(harvestApp.adapter.update.callCount).to.be.eql(2);
+      it('should write a checkpoint in a given interval', done => {
+        clock.tick(eventsReaderDebounceDelay);
+        expect(harvestApp.adapter.update.callCount).to.be.eql(1);
 
-      done();
+        checkpointEvent.emit('newCheckpoint', 1, fakeDoc);
+        clock.tick(eventsReaderDebounceDelay);
+        expect(harvestApp.adapter.update.callCount).to.be.eql(2);
+
+        done();
+      });
     });
 
   });
