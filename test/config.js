564b56564778cc0cefae510654ae63dce4d6f53c
Lucas Lago <lucasbenderlago@gmail.com>
2017-03-29 12:55:52 -0300

    Run lint task during CI build

--- test/config.js
+++ test/config.js
@@ -7,10 +7,10 @@ module.exports = {
     port: harvesterPort,
     options: {
       adapter: 'mongodb',
-      connectionString: process.env.MONGODB_URL || "mongodb://db:27017/test",
+      connectionString: process.env.MONGODB_URL || 'mongodb://db:27017/test',
       db: process.env.MONGODB || 'test',
       inflect: true,
-      oplogConnectionString: (process.env.OPLOG_MONGODB_URL || "mongodb://db:27017/local") + '?slaveOk=true',
+      oplogConnectionString: (process.env.OPLOG_MONGODB_URL || 'mongodb://db:27017/local') + '?slaveOk=true',
     }
   }
 };
